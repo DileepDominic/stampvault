4e919220563b9e1236d0f00a777a3abb6b9f7c44
dileepdominic <dileepdominic@gmail.com>
2020-05-16 13:17:57 +0530

    Read Image

---
+++ readimage.py
@@ -0,0 +1,17 @@
+#Read an image with opencv2
+
+import sys
+import cv2
+print("OpenCV version :",cv2.__version__)
+
+image1 = cv2.imread('c:\\bigidea.jpg')
+cv2.imshow('BidIdea Image', image1)
+cv2.waitKey(0) 
+
+print(image1)
+print('Type:', type(image1))
+print('Dimensions:', image1.shape)
+print('Top-left pixel RGB :', image1[0,0])
+
+print('Complete')
+
